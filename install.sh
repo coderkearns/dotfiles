63b371f7fc231a8e50f24fc7b7e79e93366e6755
coderkearns <coder.kearns@gmail.com>
2023-05-06 19:03:07 -0500

    feat: add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Aborted!"
+    exit 0
+fi;
+
+cd "$(dirname "${BASH_SOURCE}")"
+
+git pull origin main
+rsync --exclude ".git/" --exclude "README.md" --exclude "LICENSE" --exclude "install.sh" -avh --no-perms . ~
+source ~/.bash_profile
+
+echo "Dotfiles installed successfully!"
