0c6b5540b5053da5729c17d0707242d38b5cd28e
carterkearns <carter.kearns@sisu.us>
2023-06-12 14:03:02 -0500

    feat: add 'superpull' git alias

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -13,11 +13,16 @@ git config --global pull.rebase false
 
 git config --global submodule.recurse true
 
+function gitconfig-function {
+  git config --global alias.$1 "!git_$1() { $2 ;}; git_$1"
+}
+
 function gitconfig-alias {
   git config --global alias.$1 "$2"
 }
 
-gitconfig-alias ignore "!gi() { curl -sL https://www.toptal.com/developers/gitignore/api/$@ ;}; gi"
+gitconfig-function ignore "curl -sL https://www.toptal.com/developers/gitignore/api/$@"
+gitconfig-function superpull "git reset --hard origin/\$(git branch --show-current)"
 gitconfig-alias undo "reset --soft HEAD~1"
 gitconfig-alias ouch "reset --hard HEAD~1"
 gitconfig-alias typo "commit --amend"
